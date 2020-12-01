beab51685a9649ebec215fc9b1d2057d2ec3ab92
Marcos-Puglliesy <marcospuglliesyprogramer@gmail.com>
2020-12-01 16:51:46 -0300

    added auto scroll in view-messages and clean input

--- src/js/send.js
+++ src/js/send.js
@@ -1,16 +1,19 @@
-let input = document.querySelector('.msg');
+let input = document.querySelector(".msg");
 
-let view = document.querySelector('.messages-send');
+let view = document.querySelector(".messages-send");
 
-input.addEventListener('keydown', (e) => {
+input.addEventListener("keydown", (e) => {
   if (e.key === "Enter" && input.value.length >= 1) {
-    let div = document.createElement('div')
-    let p = document.createElement('p')
-    p.innerText = input.value
-    div.classList.add('msg-send')
-    div.classList.add('i')
-    div.appendChild(p)
-    
-    view.appendChild(div)
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    p.innerText = input.value;
+    div.classList.add("msg-send");
+    div.classList.add("i");
+    div.appendChild(p);
+
+    input.value = "";
+    view.appendChild(div);
+    let heightScroll = view.scrollHeight;
+    view.scroll(0, heightScroll);
   }
-})
+});
